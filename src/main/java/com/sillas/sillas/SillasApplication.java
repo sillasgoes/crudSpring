02de755ba3d6aecb7c8ea08a787c1377b0528594
Sillas <sillasgoes@gmail.com>
2025-11-07 08:14:54 -0300

    First Commit

---
+++ src/main/java/com/sillas/sillas/SillasApplication.java
@@ -0,0 +1,13 @@
+package com.sillas.sillas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SillasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SillasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sillas/sillas/SillasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sillas.sillas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SillasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
